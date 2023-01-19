150a8df048451271cc927196ce3baafab830ff33
m.md750.md <m.md750.md@gmail.com>
2023-01-18 22:55:59 -0300

    feat:create routes for add habit and get habits by date
    - Installed dependences zod, dayjs, ...;
    - Created models (tables) necessaries;
    - Created seed for add initial records;
    - Create routes for add habit and other for get habits by date;
    - Updated structure of files;

---
+++ src/routes.ts
@@ -0,0 +1,75 @@
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import dayjs from 'dayjs';
+import { prisma } from "./lib/prisma"
+
+export async function appRoutes(app: FastifyInstance) {
+    app.post('/habit', async (request) => {
+        const createHabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(
+                z.number().min(0).max(6)
+            )
+        })
+
+        const { title, weekDays } = createHabitBody.parse(request.body)
+        
+        const today = dayjs().startOf('day').toDate()
+
+        await prisma.habit.create({
+            data: {
+                title,
+                created_at: today,
+                habitWeekDays: {
+                    create: weekDays.map(weekDay => {
+                        return {
+                            week_day: weekDay
+                        }
+                    })
+                }
+            }
+        })
+    });
+
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        });
+
+        const { date } = getDayParams.parse(request.body);
+
+        const parsedDate = dayjs(date).startOf('day');
+        const weekDay = parsedDate.get('day');
+
+        const possiblehabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+                habitWeekDays: {
+                    some: {
+                        week_day: weekDay,
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate()
+            },
+            include: {
+                dayHabits: true
+            }
+        });
+
+        const completedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+        })
+
+        return {
+            possiblehabits,
+            completedHabits
+        }
+    });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,14 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+import { appRoutes } from './routes';
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get('/', async () => {
-    const hasbits = await prisma.habit.findMany()
-
-    return 'Hello World'
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333
+}).then(() => {
+    console.log('HTTP Server Running!');
 })
